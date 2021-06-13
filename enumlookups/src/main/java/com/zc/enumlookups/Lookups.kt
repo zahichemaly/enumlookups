73a8fd5a9e5ea379384d1310dda71d61f8e60ad1
zahichemaly <chemalyzahi@hotmail.com>
2021-06-13 18:32:31 +0300

    Renamed module

--- enumlookups/src/main/java/com/zc/enumlookups/Lookups.kt
+++ enumlookups/src/main/java/com/zc/enumlookups/Lookups.kt
@@ -6,6 +6,8 @@ package com.zc.enumlookups
 
 /**
  * Reverse lookup of an enum using ordinal.
+ * @param value: Ordinal value.
+ * @param default: Default enum if not found. If not specified, the first enum is returned.
  */
 inline fun <reified T : Enum<T>> lookup(value: Int, default: T = enumValues<T>().first()): T {
     return enumValues<T>().find { it.ordinal == value } ?: default
@@ -13,6 +15,9 @@ inline fun <reified T : Enum<T>> lookup(value: Int, default: T = enumValues<T>()
 
 /**
  * Reverse lookup of an enum using name.
+ * @param name: Name of enum
+ * @param default: Default enum if not found. If not specified, the first enum is returned.
+ * @param ignoreCase: `true` to ignore character case when comparing enum names.
  */
 inline fun <reified T : Enum<T>> lookupByName(name: String, default: T = enumValues<T>().first(), ignoreCase: Boolean = true): T {
     return enumValues<T>().find { it.name.contentEquals(name, ignoreCase) } ?: default
@@ -20,20 +25,22 @@ inline fun <reified T : Enum<T>> lookupByName(name: String, default: T = enumVal
 
 /**
  * Reverse lookup of an enum using a field as key.
+ * @param value: Value of enum.
  */
 inline fun <reified T : Enum<T>, V> ((T) -> V).lookupByValue(value: V, default: T = enumValues<T>().first()): T {
     return enumValues<T>().find { this(it) == value } ?: default
 }
 
 /**
- * Reverse lookup of enums using a predicate.
+ * Returns a list of enums matching the predicate.
  */
 inline fun <reified T : Enum<T>> lookupFilter(predicate: (T) -> Boolean): List<T> {
     return enumValues<T>().filter(predicate)
 }
 
 /**
- * Reverse lookup of an enum using a field as key.
+ * Reverse lookup operator. Calls [lookupByValue] with default params.
+ * @param value: Value of enum.
  */
 inline infix fun <reified T : Enum<T>, V> ((T) -> V).from(value: V): T {
     return lookupByValue(value)
